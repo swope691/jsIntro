85380fb4d8d6b20b7ac9b2a59847362ecd6c434a
Phillip Swope <ps0895423@otc.edu>
2018-09-05 00:42:07 -0500

    multiple change

--- multipleTable.js
+++ multipleTable.js
@@ -1,2 +1,13 @@
 const h = "5";
-console.log(h * 1, h * 2, h * 3, h * 4, h * 5, h * 6, h * 7, h * 8, h * 9, h * 10,   );
+
+const a = h * 1
+const b = h * 2
+const c = h * 3
+const d = h * 4
+const e = h * 5
+const f = h * 6
+const g = h * 7
+const i = h * 8
+const j = h * 9
+const k = h * 10
+console.log(a, b ,c ,d, e, f, g, i, j, k);
